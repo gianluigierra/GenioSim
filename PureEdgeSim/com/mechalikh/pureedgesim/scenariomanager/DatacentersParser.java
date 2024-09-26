46b811b5e5d19828637e6c0b25d58d7fa7c0b179
gianluigierra <gianluigierra@gmail.com>
2024-09-27 00:57:25 +0200

    Da qui inizia la modifica aggiungendo le VM al progetto. Il precedente commit non aggiungeva praticamente nulla, aveva solo migliorato la formattazione del codice. Da qua in poi si aggiungono cose.

--- PureEdgeSim/com/mechalikh/pureedgesim/scenariomanager/DatacentersParser.java
+++ PureEdgeSim/com/mechalikh/pureedgesim/scenariomanager/DatacentersParser.java
@@ -8,6 +8,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.mechalikh.pureedgesim.scenariomanager.SimulationParameters.TYPES;
+import com.mechalikh.pureedgesim.simulationmanager.SimLog;
 
 public class DatacentersParser extends ComputingNodesParser {
 
@@ -21,12 +22,8 @@ public class DatacentersParser extends ComputingNodesParser {
 		for (int i = 0; i < datacenterList.getLength(); i++) {
 			Node datacenterNode = datacenterList.item(i);
 			Element datacenterElement = (Element) datacenterNode;
-			for (String element : List.of("isOrchestrator", "idleConsumption", "maxConsumption", "cores", "mips", "ram",
-					"storage"))
+			for (String element : List.of("isOrchestrator", "idleConsumption", "maxConsumption"))
 				isElementPresent(datacenterElement, element);
-
-			for (String element : List.of("cores", "mips", "ram", "storage"))
-				assertDouble(datacenterElement, element, value -> (value > 0), "> 0. Check the file: " + file );
 
 			assertDouble(datacenterElement, "idleConsumption", value -> (value >= 0),
 					">= 0. Check the file " + file);
@@ -35,21 +32,72 @@ public class DatacentersParser extends ComputingNodesParser {
 			assertDouble(datacenterElement, "maxConsumption", value -> (value > idleConsumption),
 					"> \"idleConsumption\". Check the file " + file);
 
+			isElementPresent(datacenterElement, "hosts");
+			isElementPresent(datacenterElement, "host");
+			NodeList hostList = datacenterElement.getElementsByTagName("host");
+			for (int j = 0; j < hostList.getLength(); j++) {
+				Node hostNode = hostList.item(j);
+
+				Element hostElement = (Element) hostNode;
+
+				
+				for (String element : List.of("cores", "mips", "ram", "storage"))
+					isElementPresent(datacenterElement, element);
+
+				for (String element : List.of("cores", "mips", "ram", "storage"))
+					assertDouble(datacenterElement, element, value -> (value > 0), "> 0. Check the file: " + file);
+
+				double Hostcores = Double.parseDouble(datacenterElement.getElementsByTagName("cores").item(0).getTextContent());
+				double Hostmips = Double.parseDouble(datacenterElement.getElementsByTagName("mips").item(0).getTextContent());
+				double Hostram = Double.parseDouble(datacenterElement.getElementsByTagName("ram").item(0).getTextContent());
+				double Hoststorage = Double.parseDouble(datacenterElement.getElementsByTagName("storage").item(0).getTextContent());
+
+				double cores = 0;
+				double mips = 0;
+				double ram = 0;
+				double storage = 0;
+				
+				NodeList vmList = hostElement.getElementsByTagName("VM");
+				for (int k = 0; k < vmList.getLength(); k++) {
+					Node vmNode = vmList.item(k);
+
+					Element vmElement = (Element) vmNode;
+					isElementPresent(vmElement, "cores");
+					isElementPresent(vmElement, "mips");
+					isElementPresent(vmElement, "ram");
+					isElementPresent(vmElement, "storage");
+
+					cores += Double.parseDouble(vmElement.getElementsByTagName("cores").item(0).getTextContent());
+					mips += Double.parseDouble(vmElement.getElementsByTagName("mips").item(0).getTextContent());
+					ram += Double.parseDouble(vmElement.getElementsByTagName("ram").item(0).getTextContent());
+					storage += Double.parseDouble(vmElement.getElementsByTagName("storage").item(0).getTextContent());
+
+				}
+
+				System.out.println(Hostcores + " " + cores);
+
+				if(Hostcores < cores) throw new IllegalArgumentException( getClass().getSimpleName() + " - Error, the sum of cores of VMs must be lower-equal than the cores of the Host!");
+				if(Hostmips < mips)  throw new IllegalArgumentException( getClass().getSimpleName() + " - Error, the sum of Mips of VMs must be lower-equal to the Mips of the Host!");
+				if(Hostram < ram)  throw new IllegalArgumentException( getClass().getSimpleName() + " - Error, the sum of ram of VMs must be lower-equal than the ram of the Host!\"");
+				if(Hoststorage < storage)  throw new IllegalArgumentException( getClass().getSimpleName() + " - Error, the sum of storage of VMs must be lower-equal than the storage of the Host!");
+
+			}
+
 			if (type == TYPES.CLOUD) {
 				SimulationParameters.numberOfCloudDataCenters++;
-				Element location = (Element) datacenterElement.getElementsByTagName("location").item(0);					//MODIFICA MIA, non erano presenti queste info sul cloud
-				isElementPresent(location, "x_pos");																			//originariamente la posizione non era necessaria sul cloud
-				isElementPresent(location, "y_pos");																			//Luciano però l'ha aggiunta, quindi la parsizzo	
-				assertDouble(location, "x_pos", value -> (value >= 0), ">= 0. Check the " + file + " file!");				//
-				assertDouble(location, "y_pos", value -> (value > 0), "> 0. Check the " + file + " file!");				//
-			} else if (type == TYPES.EDGE_DATACENTER){																				//MODIFICA MIA, qui non era presente l'if. Era solo else
+				Element location = (Element) datacenterElement.getElementsByTagName("location").item(0); 		// MODIFICA MIA, non erano presenti queste info sul cloud
+				isElementPresent(location, "x_pos"); 																	// originariamente la posizione non era necessaria sul cloud
+				isElementPresent(location, "y_pos"); 																	// Luciano però l'ha aggiunta, quindi la parsizzo
+				assertDouble(location, "x_pos", value -> (value >= 0), ">= 0. Check the " + file + " file!"); 	//
+				assertDouble(location, "y_pos", value -> (value > 0), "> 0. Check the " + file + " file!"); 		//
+			} else if (type == TYPES.EDGE_DATACENTER) { 																	// MODIFICA MIA, qui non era presente l'if. Era solo else
 				SimulationParameters.numberOfEdgeDataCenters++;
 				Element location = (Element) datacenterElement.getElementsByTagName("location").item(0);
 				isElementPresent(location, "x_pos");
 				isElementPresent(location, "y_pos");
 				assertDouble(location, "x_pos", value -> (value >= 0), ">= 0. Check the " + file + " file!");
 				assertDouble(location, "y_pos", value -> (value > 0), "> 0. Check the " + file + " file!");
-			} 
+			}
 
 		}
 		return true;
