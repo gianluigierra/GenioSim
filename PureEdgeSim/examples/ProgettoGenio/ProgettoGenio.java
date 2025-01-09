22059c559055616f44c0f11b761af7a12d614dd6
gianluigierra <gianluigierra@gmail.com>
2025-01-09 17:10:39 +0100

    Implementato gli scenari da simulare

--- PureEdgeSim/examples/ProgettoGenio/ProgettoGenio.java
+++ PureEdgeSim/examples/ProgettoGenio/ProgettoGenio.java
@@ -27,14 +27,15 @@ public class ProgettoGenio {
 		sim.setCustomSettingsFolder(settingsPath);
 
 		// cambio la modalità di esecuzione
-		String exampleMode = "Scenario_1";
+		String exampleMode = "SmartCity";
 
 		switch (exampleMode) {
-			case "Smart_Lights":
-				sim.setSimulationParameterProperties(settingsPath, "Smart_Lights/simulation_parameters_Smart_Lights.properties");
-				sim.setApplicationsXML(settingsPath, "Smart_Lights/applications_Smart_Lights.xml");
-				sim.setEdgeDatacentersXML(settingsPath, "Smart_Lights/edge_datacenters_Smart_Lights.xml");
-				sim.setCloudDatacentersXML(settingsPath, "Smart_Lights/cloud_Smart_Lights.xml");
+			case "SmartCity":
+				sim.setSimulationParameterProperties(settingsPath, "SmartCity/simulation_parameters_SmartCity.properties");
+				sim.setApplicationsXML(settingsPath, "SmartCity/applications_SmartCity.xml");
+				sim.setUsersXML(settingsPath, "SmartCity/users.xml");
+				sim.setEdgeDatacentersXML(settingsPath, "SmartCity/edge_datacenters_SmartCity.xml");
+				sim.setCloudDatacentersXML(settingsPath, "SmartCity/cloud_SmartCity.xml");
 				sim.setSimulationName(exampleMode);
 				break;
 		
@@ -47,14 +48,24 @@ public class ProgettoGenio {
 				sim.setSimulationName(exampleMode);
 				break;
 		
-			case "Video_Surveillance":
-				sim.setSimulationParameterProperties(settingsPath, "Video_Surveillance/simulation_parameters_Video_Surveillance.properties");
-				sim.setApplicationsXML(settingsPath, "Video_Surveillance/applications_Video_Surveillance.xml");
-				sim.setUsersXML(settingsPath, "Video_Surveillance/users.xml");
-				sim.setEdgeDatacentersXML(settingsPath, "Video_Surveillance/edge_datacenters_Video_Surveillance.xml");
-				sim.setCloudDatacentersXML(settingsPath, "Video_Surveillance/cloud_Video_Surveillance.xml");
+			case "Smart_Building":
+				sim.setSimulationParameterProperties(settingsPath, "Smart_Building/simulation_parameters_Smart_Building.properties");
+				sim.setApplicationsXML(settingsPath, "Smart_Building/applications_Smart_Building.xml");
+				sim.setUsersXML(settingsPath, "Smart_Building/users.xml");
+				sim.setEdgeDatacentersXML(settingsPath, "Smart_Building/edge_datacenters_Smart_Building.xml");
+				sim.setCloudDatacentersXML(settingsPath, "Smart_Building/cloud_Smart_Building.xml");
 				sim.setSimulationName(exampleMode);
 				break;
+
+			case "Scenario_Misto":
+				sim.setSimulationParameterProperties(settingsPath, "Scenario_Misto/simulation_parameters_Scenario_Misto.properties");
+				sim.setApplicationsXML(settingsPath, "Scenario_Misto/applications_Scenario_Misto.xml");
+				sim.setUsersXML(settingsPath, "Scenario_Misto/users_Scenario_Misto.xml");
+				sim.setEdgeDatacentersXML(settingsPath, "Scenario_Misto/edge_datacenters_Scenario_Misto.xml");
+				sim.setCloudDatacentersXML(settingsPath, "Scenario_Misto/cloud_Scenario_Misto.xml");
+				sim.setONTsXML(settingsPath, "Scenario_Misto/ONT_Scenario_Misto.xml");
+				sim.setSimulationName(exampleMode);
+				break;	
 
 			case "Scenario_1":
 				sim.setSimulationParameterProperties(settingsPath, "Scenario_1/simulation_parameters_Scenario_1.properties");
